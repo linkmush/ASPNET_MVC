79ac95d22784815f06ef6237435fea84c283c425
oskar lindqvist <lindqvist_11@hotmail.com>
2024-04-16 05:54:54 +0700

    contactrequest works

--- WebAppMVC/Controllers/ContactController.cs
+++ WebAppMVC/Controllers/ContactController.cs
@@ -1,12 +1,62 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Text;
+using WebAppMVC.ViewModels.Views;
+using static System.Net.WebRequestMethods;
 
 namespace WebAppMVC.Controllers;
 
-public class ContactController : Controller
+public class ContactController(HttpClient http, IConfiguration configuration) : Controller
 {
+    private readonly HttpClient _http = http;
+    private readonly IConfiguration _configuration = configuration;
+
+    [Route("/contact")]
     public IActionResult Index()
     {
+        var contactViewModel = new ContactsViewModel();
         ViewData["Title"] = "Contact Us";
-        return View();
+        return View(contactViewModel);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SubmitContact(ContactsViewModel viewModel)
+    {
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var content = new StringContent(JsonConvert.SerializeObject(viewModel), Encoding.UTF8, "application/json");
+                    var response = await _http.PostAsync($"https://localhost:7091/api/contacts?key={_configuration["ApiKey"]}", content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        ViewData["Status"] = "Success";
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+                    {
+                        ViewData["Status"] = "AlreadyExists";
+                    }
+                    else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                    {
+                        ViewData["Status"] = "Unauthorized";
+                    }
+                }
+                catch
+                {
+                    ViewData["Status"] = "ConnectionFailed";
+                }
+            }
+            else
+            {
+                ViewData["Status"] = "Invalid";
+            }
+
+            var contactViewModel = new ContactsViewModel();
+
+            return View("Index", contactViewModel);
+        }
     }
 }
---
+++ WebAppMVC/ViewModels/Views/ContactsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppMVC.ViewModels.Views;
+
+public class ContactsViewModel
+{
+    [DataType(DataType.Text)]
+    [Display(Name = "Full name", Prompt = "Enter your full name", Order = 0)]
+    [Required(ErrorMessage = "Full name is required")]
+    [MinLength(2, ErrorMessage = "Full name should be at least 5 characters")]
+    public string FullName { get; set; } = null!;
+
+    [Display(Name = "Email address", Prompt = "Enter your email address", Order = 2)]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Email is required")]
+    [RegularExpression("^[^@\\s]+@[^@\\s]+\\.[^@\\s]{2,}$", ErrorMessage = "Your email address is invalid")]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Service")]
+    public string? Service { get; set; }
+
+    [Display(Name = "Message", Prompt = "Enter your message here...")]
+    [Required(ErrorMessage = "Message is required")]
+    public string Message { get; set; } = null!;
+}
